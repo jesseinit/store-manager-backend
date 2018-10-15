86bfbdff0dbf6104d7fbcee8bd3ac8088645bcba
Jesse Egbosionu <j3bsie@gmail.com>
2018-10-15 14:28:52 +0100

    feat(API): Implement PUT /products/:id route
    - Update route handler class and methods with JSDoc style comments
    - Validate user input with validator mechanism
    - Write route logic
    - Update test script to cover funtionality
    
    [Finishes 161201478]

--- server/middleware/inputValidator.js
+++ server/middleware/inputValidator.js
@@ -24,7 +24,34 @@ const validateNewProduct = [
     .withMessage('Product name must be atlease 2 letters long'),
   body('price')
     .isInt({ min: 1 })
-    .withMessage('Product price must be decimal number of 1.00 or more'),
+    .withMessage('Product price must be decimal number of 1 or more'),
+  body('qty')
+    .isNumeric({ min: 1 })
+    .withMessage('Product qty must be a positive number from 1')
+];
+
+const validateProductUpdate = [
+  param('id')
+    .isInt({ min: 1 })
+    .withMessage('Product ID must be a positve number from 1'),
+  body('imgUrl')
+    .custom(imageUrl => {
+      const checkUrl = /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)/g;
+      return checkUrl.test(imageUrl);
+    })
+    .withMessage('Image Input should be a valid image url'),
+  body('name')
+    .isString()
+    .isLength({ min: 2 })
+    .withMessage('Product name must be atlease 2 letters long'),
+  body('category')
+    .isString()
+    .withMessage('Product category must be a string')
+    .isLength({ min: 2 })
+    .withMessage('Product name must be atlease 2 letters long'),
+  body('price')
+    .isInt({ min: 1 })
+    .withMessage('Product price must be decimal number of 1 or more'),
   body('qty')
     .isNumeric({ min: 1 })
     .withMessage('Product qty must be a positive number from 1')
@@ -33,15 +60,14 @@ const validateNewProduct = [
 const validationHandler = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    res
-      .status(422)
-      .json({ error: errors.array() /* .map(error => error.msg) */ });
+    res.status(422).json({ error: errors.array().map(error => error.msg) });
   } else {
     next();
   }
 };
 
 const validations = {
+  validateProductUpdate,
   validateNewProduct,
   validateProductId,
   validationHandler
--- server/test/test.js
+++ server/test/test.js
@@ -7,93 +7,170 @@ chai.use(chaiHttp);
 
 describe('Store Manager', () => {
   describe('Products', () => {
-    it('Users should be able to get all product', done => {
-      chai
-        .request(app)
-        .get('/api/v1/products/')
-        .end((err, res) => {
-          expect(res.status).to.equal(200);
-          done(err);
-        });
+    describe('GET /products', () => {
+      it('Users should be able to get all product', done => {
+        chai
+          .request(app)
+          .get('/api/v1/products/')
+          .end((err, res) => {
+            expect(res.status).to.equal(200);
+            done(err);
+          });
+      });
     });
 
-    it('It should return an Unprocessable Input error when user pass in an invalid product id', done => {
-      chai
-        .request(app)
-        .get('/api/v1/products/*')
-        .end((err, res) => {
-          expect(res.status).to.equal(422);
-          expect(res.body).to.have.property('error');
-          done(err);
-        });
+    describe('GET /products/:id', () => {
+      it('Invalid product id should return an unprocessable input error', done => {
+        chai
+          .request(app)
+          .get('/api/v1/products/*')
+          .end((err, res) => {
+            expect(res.status).to.equal(422);
+            expect(res.body).to.have.property('error');
+            done(err);
+          });
+      });
+
+      it('Non-existing product id should return a not found error when fetching product', done => {
+        chai
+          .request(app)
+          .get('/api/v1/products/5')
+          .end((err, res) => {
+            expect(res.status).to.equal(404);
+            expect(res.body).to.not.have.property('result');
+            done(err);
+          });
+      });
+
+      it('Valid product id should return the matching product', done => {
+        chai
+          .request(app)
+          .get('/api/v1/products/1')
+          .end((err, res) => {
+            expect(res.status).to.equal(200);
+            expect(res.body).to.have.property('result');
+            expect(res.body.result).to.have.length(1);
+            done(err);
+          });
+      });
     });
 
-    it('It should return a Not Found response when user pass in an invalid product id', done => {
-      chai
-        .request(app)
-        .get('/api/v1/products/5')
-        .end((err, res) => {
-          expect(res.status).to.equal(404);
-          expect(res.body).to.not.have.property('result');
-          done(err);
-        });
+    describe('POST /products', () => {
+      it('Invalid product id should return an unprocessable input error', done => {
+        chai
+          .request(app)
+          .post('/api/v1/products')
+          .send({
+            imgUrl: 1027,
+            name: '32inch LED Television',
+            category: 'Electronics',
+            price: '78900',
+            qty: 0
+          })
+          .end((err, res) => {
+            expect(res.status).to.equal(422);
+            expect(res.body).to.have.property('error');
+            done(err);
+          });
+      });
+
+      it('Admin should be able to create a product', done => {
+        chai
+          .request(app)
+          .post('/api/v1/products')
+          .send({
+            imgUrl: 'https://example.com/tv.jpg',
+            name: '32inch LED Television',
+            category: 'Electronics',
+            price: 78900,
+            qty: 5
+          })
+          .end((err, res) => {
+            expect(res.status).to.equal(201);
+            expect(res.body).to.have.property('result');
+            expect(res.body.result).to.have.length(1);
+            expect(res.body.result[0]).to.have.all.keys(
+              'id',
+              'imgUrl',
+              'name',
+              'category',
+              'price',
+              'qty'
+            );
+            done(err);
+          });
+      });
     });
 
-    it('Users should be able to view a single product', done => {
-      chai
-        .request(app)
-        .get('/api/v1/products/1')
-        .end((err, res) => {
-          expect(res.status).to.equal(200);
-          expect(res.body).to.have.property('result');
-          expect(res.body.result).to.have.length(1);
-          done(err);
-        });
-    });
+    describe('PUT /products/:id', () => {
+      it('Invalid product details information should return an unprocessable input error during product update', done => {
+        chai
+          .request(app)
+          .put('/api/v1/products/5')
+          .send({
+            imgUrl: 1027,
+            name: '32inch LED Television',
+            category: 'Electronics',
+            price: '78900',
+            qty: 0
+          })
+          .end((err, res) => {
+            expect(res.status).to.equal(422);
+            expect(res.body).to.have.property('error');
+            done(err);
+          });
+      });
 
-    it('It should return an Unprocessable Input error when user pass in an invalid product details', done => {
-      chai
-        .request(app)
-        .post('/api/v1/products')
-        .send({
-          imgUrl: 1027,
-          name: '32inch LED Television',
-          category: 'Electronics',
-          price: '78900',
-          qty: 0
-        })
-        .end((err, res) => {
-          expect(res.status).to.equal(422);
-          expect(res.body).to.have.property('error');
-          done(err);
-        });
-    });
+      it('Non-existing product id should return a not found error during product update', done => {
+        chai
+          .request(app)
+          .put('/api/v1/products/10')
+          .send({
+            imgUrl: 'https://example.com/tv.jpg',
+            name: '32inch LED Television',
+            category: 'Electronics',
+            price: 78900,
+            qty: 5
+          })
+          .end((err, res) => {
+            expect(res.status).to.equal(404);
+            done(err);
+          });
+      });
 
-    it('Admin should be able to create a product', done => {
-      chai
-        .request(app)
-        .post('/api/v1/products')
-        .send({
-          imgUrl: 'https://example.com/tv.jpg',
-          name: '32inch LED Television',
-          category: 'Electronics',
-          price: 78900,
-          qty: 5
-        })
-        .end((err, res) => {
-          expect(res.status).to.equal(201);
-          expect(res.body).to.have.property('result');
-          expect(res.body.result).to.have.length(1);
-          expect(res.body.result[0]).to.have.all.keys(
-            'id',
-            'imgUrl',
-            'name',
-            'category',
-            'price',
-            'qty'
-          );
-          done(err);
-        });
+      it('Product should update with same product details when there are no input errors', done => {
+        chai
+          .request(app)
+          .put('/api/v1/products/5')
+          .send({
+            imgUrl: 'https://example.com/tv.jpg',
+            name: '32inch LED Television',
+            category: 'Electronics',
+            price: 78900,
+            qty: 5
+          })
+          .end((err, res) => {
+            expect(res.status).to.equal(200);
+            done(err);
+          });
+      });
+
+      it('Product should update with different product details when there are no input errors', done => {
+        chai
+          .request(app)
+          .put('/api/v1/products/5')
+          .send({
+            imgUrl: 'https://example.com/tv1.jpg',
+            name: '42inch LED Television',
+            category: 'Electronics.',
+            price: 77900,
+            qty: 6
+          })
+          .end((err, res) => {
+            expect(res.status).to.equal(200);
+            done(err);
+          });
+      });
     });
   });
 });
