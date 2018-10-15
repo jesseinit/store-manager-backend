461d5ea4064d82cb970990cf16857c72172b446e
Jesse Egbosionu <j3bsie@gmail.com>
2018-10-15 01:18:33 +0100

    feat(API): Implement GET /products/:id functionality
    - Update Readme with CI badges
    - Write test
    
    [Starts 161201476]

--- server/test/test.js
+++ server/test/test.js
@@ -15,4 +15,38 @@ describe('Store Manager', () => {
         done(err);
       });
   });
+
+  it('It should return an Unprocessable Input error when user pass in an invalid product id', done => {
+    chai
+      .request(app)
+      .get('/api/v1/products/*')
+      .end((err, res) => {
+        expect(res.status).to.equal(422);
+        expect(res.body).to.have.property('error');
+        done(err);
+      });
+  });
+
+  it('It should return a Not Found response when user pass in an invalid product id', done => {
+    chai
+      .request(app)
+      .get('/api/v1/products/5')
+      .end((err, res) => {
+        expect(res.status).to.equal(404);
+        expect(res.body.result).to.have.length(0);
+        done(err);
+      });
+  });
+
+  it('Users should be able to view a single product', done => {
+    chai
+      .request(app)
+      .get('/api/v1/products/1')
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body).to.have.property('result');
+        expect(res.body.result).to.have.length(1);
+        done(err);
+      });
+  });
 });
