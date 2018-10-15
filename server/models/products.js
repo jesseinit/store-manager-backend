76d0acf5e173bd621e5282eae44a213a4b5b62de
Jesse Egbosionu <j3bsie@gmail.com>
2018-10-15 06:29:57 +0100

    feat(API): Implement POST /products functionality
    - Add category to products model
    - Write tests

--- server/models/products.js
+++ server/models/products.js
@@ -3,6 +3,7 @@ const products = [
     id: 1,
     imgUrl: 'photo.jpg',
     name: 'Television',
+    category: 'Electronics',
     qty: 13,
     price: 78500
   },
@@ -10,6 +11,7 @@ const products = [
     id: 2,
     imgUrl: 'photo.jpg',
     name: 'PlayStaytion 4',
+    category: 'Gaming',
     qty: 11,
     price: 150000
   },
@@ -17,6 +19,7 @@ const products = [
     id: 3,
     imgUrl: 'photo.jpg',
     name: 'Iphone X',
+    category: 'Mobile Phones',
     qty: 10,
     price: 90000
   },
@@ -24,6 +27,7 @@ const products = [
     id: 4,
     imgUrl: 'photo.jpg',
     name: 'Fridge',
+    category: 'Electronics',
     qty: 9,
     price: 65800
   }
--- server/test/test.js
+++ server/test/test.js
@@ -50,5 +50,49 @@ describe('Store Manager', () => {
           done(err);
         });
     });
+
+    it('It should return an Unprocessable Input error when user pass in an invalid product details', done => {
+      chai
+        .request(app)
+        .post('/api/v1/products')
+        .send({
+          imgUrl: 1027,
+          name: '32inch LED Television',
+          category: 'Electronics',
+          price: '78900',
+          qty: '5'
+        })
+        .end((err, res) => {
+          expect(res.status).to.equal(422);
+          expect(res.body).to.have.property('error');
+          done(err);
+        });
+    });
+
+    it('Admin should be able to create a product', done => {
+      chai
+        .request(app)
+        .post('/api/v1/products')
+        .send({
+          imgUrl: 'product-image.jpeg',
+          name: '32inch LED Television',
+          category: 'Electronics',
+          price: 78900,
+          qty: 5
+        })
+        .end((err, res) => {
+          expect(res.status).to.equal(201);
+          expect(res.body).to.have.property('result');
+          expect(res.body.result).to.have.length(1);
+          expect(res.body.result[0]).to.have.all.keys(
+            'imgUrl',
+            'name',
+            'category',
+            'price',
+            'qty'
+          );
+          done(err);
+        });
+    });
   });
 });
