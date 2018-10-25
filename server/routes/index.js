2a57274f0a6fd4f4fa70812f0f461e8394a9defe
Jesse Egbosionu <j3bsie@gmail.com>
2018-10-25 22:29:17 +0100

    - Update userController logic flow - Remove route error middleware

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,9 +7,4 @@ const router = Router();
 
 router.use('/api/v1', user, products, sales);
 
-router.use((err, req, res, next) => {
-  res.status(err.status).json({ status: false, message: err.message });
-  next();
-});
-
 export default router;
