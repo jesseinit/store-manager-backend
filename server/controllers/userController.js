5e8cae2774d9cf22324263e31a55b77b28005fa4
Jesse Egbosionu <j3bsie@gmail.com>
2018-10-26 21:26:49 +0100

    - Update userController

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,11 @@
-import UserHelper from '../helpers/userHelper';
+import 'babel-polyfill';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+// import UserHelper from '../helpers/userHelper';
+import pool from '../utils/connection';
+import query from '../utils/queries';
+
+const SECRET_KEY = process.env.JWT_KEY;
 
 /**
  *
@@ -16,14 +23,31 @@ class UserController {
    * @returns {string} User token
    * @memberof UserController
    */
-  static async loginUser(req, res, next) {
+  static async loginUser(req, res) {
     const { userid, password } = req.body;
-    const result = await UserHelper.loginUser({ userid, password });
-    if (result instanceof Error) {
-      next(result);
+    const foundUser = await pool.query(query.findUser(userid));
+
+    if (foundUser.rows.length < 1) {
+      res.status(404).send({ status: false, message: 'User not found' });
       return;
     }
-    res.status(200).json({ status: true, token: result });
+
+    const isPasswordValid = await bcrypt.compare(password, foundUser.rows[0].password);
+    if (!isPasswordValid) {
+      res.status(401).send({ status: false, message: 'Authenication Failed' });
+      return;
+    }
+
+    const token = jwt.sign(
+      {
+        id: foundUser.rows[0].userid,
+        name: foundUser.rows[0].name,
+        role: foundUser.rows[0].role
+      },
+      SECRET_KEY
+    );
+
+    res.status(200).json({ status: true, token });
   }
 }
 
